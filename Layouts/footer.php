378a329af23c67e5009b62fb014f8416fb3408f1
akangread <siu419742@gmail.com>
2025-05-04 21:27:32 +0700

    first commit

--- Layouts/footer.php
+++ Layouts/footer.php
@@ -3,7 +3,7 @@
     <strong>Copyright &copy; <?= date("Y") ?> <a href="https://alwafisysdev.tech">erkaskuy</a>.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
-        <b>Version</b> 3.2.0
+        <b>Student of STT NF </b> Angkatan 2024
     </div>
 </footer>
 
--- Layouts/navbar.php
+++ Layouts/navbar.php
@@ -146,7 +146,7 @@ $activeUrl = $_GET['url'] ?? ''; // Get the current URL parameter or default to 
     <a href="index3.html" class="brand-link">
         <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
             style="opacity: .8">
-        <span class="brand-text font-weight-light">AdminLTE 3</span>
+        <span class="brand-text font-weight-light">Puskesmas Buah Hati</span>
     </a>
 
     <!-- Sidebar -->
@@ -166,7 +166,7 @@ $activeUrl = $_GET['url'] ?? ''; // Get the current URL parameter or default to 
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                 <!-- Add icons to the links using the .nav-icon class -->
-                <li class="nav-header">LIST DATA</li>
+                <li class="nav-header">LIST MENU</li>
                 <li class="nav-item">
                     <a href="./?url=periksa" class="nav-link <?php echo $activeUrl === 'periksa' ? 'active' : ''; ?>">
                         <i class="nav-icon fas fa-notes-medical"></i> <!-- Icon for Periksa -->
